881822161268f805a1ad83871ad3d941d4a65a4d
Igor0400 <ulanov.igor0400@gmail.com>
2026-03-05 16:17:53 +0300

    feat: add b-cdn.net

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,33 @@ const nextConfig: NextConfig = {
     /* другие опции конфига */
     images: {
         remotePatterns: [
-            { protocol: 'https', hostname: 'wnmarathon.runc.run', pathname: '/**' },
+            {
+                protocol: 'https',
+                hostname: 'wnmarathon.runc.run',
+                pathname: '/**',
+            },
             { protocol: 'https', hostname: 'runc.run', pathname: '/**' },
-            { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '/**',
+            },
             { protocol: 'https', hostname: 'shop.runc.run', pathname: '/**' },
-            { protocol: 'https', hostname: 'магниткабежит.рф', pathname: '/**' },
-            { protocol: 'https', hostname: 'xn--80aadgmgkenqn5dg.xn--p1ai', pathname: '/**' },
+            {
+                protocol: 'https',
+                hostname: 'magnitkarun.b-cdn.net',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'магниткабежит.рф',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'xn--80aadgmgkenqn5dg.xn--p1ai',
+                pathname: '/**',
+            },
             { protocol: 'http', hostname: 'localhost', pathname: '/**' },
             { protocol: 'https', hostname: 'localhost', pathname: '/**' },
         ],
